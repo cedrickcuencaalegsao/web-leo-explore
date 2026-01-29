2aa3ddab7156639450d5c521e610dadd95a1e8da
Cedrick Alegsao <cedrickcuencaalegsao@gmail.com>
2026-01-29 11:18:13 +0800

    adding nav bar.

---
+++ app/components/navbar.tsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Leo Explorer Navbar</h1>
+      <nav className="nav-menu">Home</nav>
+      <nav className="nav-menu">Features</nav>
+      <nav className="nav-menu">Pricing</nav>
+      <nav className="nav-menu">Reviews</nav>
+      <nav className="nav-menu">Download</nav>
+      <nav className="nav-menu">Developer</nav>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-import Image from "next/image";
-
+import React from "react";
+import Navbar from "./components/navbar";
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1 className="text-3xl font-bold underline">Hello, Leo Explorer!</h1>
     </div>
   );
