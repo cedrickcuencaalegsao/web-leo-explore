575fd53fd76783d2f772e3c2d48db8349bcab5b8
Cedrick Alegsao <cedrickcuencaalegsao@gmail.com>
2026-02-11 10:50:13 +0800

    navbar ui done here.

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,13 +1,36 @@
+import React from "react";
+import Image from "next/image";
+import "./navbar.css";
+
 export default function Navbar() {
   return (
-    <div>
-      <h1 className="text-2xl font-bold">Leo Explorer Navbar</h1>
-      <nav className="nav-menu">Home</nav>
-      <nav className="nav-menu">Features</nav>
-      <nav className="nav-menu">Pricing</nav>
-      <nav className="nav-menu">Reviews</nav>
-      <nav className="nav-menu">Download</nav>
-      <nav className="nav-menu">Developer</nav>
-    </div>
+    <header className="navbar-wrapper">
+      <div className="navbar-container">
+        <div className="logo-section">
+          <Image
+            src="/icons/app-icon.png"
+            alt="Leo Explorer Logo"
+            width={32}
+            height={32}
+            className="logo-img"
+            priority
+          />
+          <span className="logo-text">Leo Explorer</span>
+        </div>
+
+        <nav className="nav-links">
+          <a href="#">Home</a>
+          <a href="#">Features</a>
+          <a href="#">Pricing</a>
+          <a href="#">Reviews</a>
+          <a href="#">Download</a>
+          <a href="#">Developers</a>
+        </nav>
+
+        <div className="cta-section">
+          <button className="btn-signup">Sign Up / Get Stated</button>
+        </div>
+      </div>
+    </header>
   );
 }
